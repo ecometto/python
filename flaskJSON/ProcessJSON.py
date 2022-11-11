b304874b022f594c9306950875a63ed3efb0d1ac
ecometto <ecometto@hotmail.comm>
2022-11-11 16:53:15 -0300

    filter country

--- flaskJSON/ProcessJSON.py
+++ flaskJSON/ProcessJSON.py
@@ -1,7 +1,9 @@
 import json
 import requests
+import re
 
 
+#obteniendo datos de API y almacendando en archivo JSON
 def downloadingData():
     ruta="./flaskJSON/countries.json"
     url="https://restcountries.com/v2/all"
@@ -12,7 +14,7 @@ def downloadingData():
         json.dump(data, file)
         
         
-#geting data from json file
+#geting data"""FROM JSON FILE"""
 def getData():
     ruta="./flaskJSON/countries.json"
     with open(ruta) as file:
@@ -20,11 +22,45 @@ def getData():
         return users
 
 
-def validar(country, DDBB ):
-    for cada in DDBB:
-        # print(cada['name'])
-        if country in cada['name']:
-            return f"existe {cada['name']}. Su capital es {cada['capital']} - Región: {cada['region']}"
-        
-res=validar("Argentina", getData())
-print(res)
+# def searchCountry(country):
+#     data=getData()
+#     for cada in data:
+#        res= re.search(country, cada['name'], re.IGNORECASE)
+#        if res:
+#            print(res)
+# searchCountry("argentina")
+
+# datos=getData()
+# def pares(country):
+#         if "Argentina" in country['name']:
+#             return country['name']
+    
+# new= map(pares, datos)
+# for cada in new:
+#     # print(list(cada))
+#     print(cada)
+
+# Return double of n
+def addition(n):
+    if "Ar" in n['name']:
+        return n
+    else:
+        return ""
+  
+# We double all numbers using map()
+numbers = getData()
+result = filter(addition, numbers)
+# print(list(result))
+for cada in result:
+    print(cada['name'])
+
+#validando usuario
+def validarUser(user, passw):
+    file="./flaskJSON/users.json"
+    with open(file) as f:
+        users= json.load(f)
+        for cada in users['users']:
+            patronUser =f"\\b{user}\\b"
+            patronPass =f"\\b{passw}\\b"
+            if re.match( patronUser , cada['nombre']) and re.match(patronPass, cada['pass']):
+                return (cada)
--- flaskJSON/app.py
+++ flaskJSON/app.py
@@ -1,25 +1,13 @@
 from flask import Flask, render_template, session, redirect, request
 
-import json
-
-#geting data from json file
-ruta="flaskJSON/users.json"
-with open(ruta) as file:
-    users= json.load(file)['data']
-
-def validar(user, password, DDBB):
-    for cada in DDBB:
-        print(cada['nombre'])
-        if user in cada['nombre'] and password in cada['pass']:
-            print("encontrado")
-            return user
-            
-usuario= validar("otadmin","123",users)
-print(usuario)
+import ProcessJSON
 
 app=Flask(__name__)
 app.secret_key="secreta"
 
+
+#geting data from json file
+data=ProcessJSON.getData()
 
 
 # ------------- rutas ------------------
@@ -30,28 +18,33 @@ def validarUser():
     if "user" not in session and request.endpoint != 'login' and '/static/' not in request.path:
         return redirect('/login')
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        us = request.form['user']
+        pa = request.form['pass']
+        res=ProcessJSON.validarUser(us,pa)
+        if res:
+            session['user'] = us
+            session['type']= res['type']
+            return redirect('/')
+
+    return render_template('login.html')
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    nombre = ""
-    if "user" in session:
-        nombre = session['user']
-    return render_template('index.html', title="principal", user=nombre)
-
+    return render_template('index.html', title="Principal", data=data)
 
 @app.route('/logout', methods=['GET', 'POST'])
 def logout():
     session.clear()
     return redirect('/login')
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        session['user']="Edgardo"
-        return redirect('/')
-    
-    
-    return render_template('login.html')
+@app.route('/search', methods=['GET', 'POST'])
+def search():
+    pass
+
+
 
 
 
